6766402b85c38dc7856378ff2c2d443b3447f937
iullian1919 <127934021+iullian1919@users.noreply.github.com>
2024-01-14 12:22:25 +0200

    Fourth commit

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 import classes from './ImageGallery.module.css';
 import PropTypes from 'prop-types';
 
 const ImageGallery = ({ images }) => {
+  useEffect(() => {
+    const uniqueIds = new Set(images.map(img => img.id));
+    console.log('Numărul total de imagini:', images.length);
+    console.log('Numărul unic de id-uri:', uniqueIds.size);
+
+    if (images.length !== uniqueIds.size) {
+      console.warn('Există id-uri duplicate în array-ul de imagini!');
+    }
+  }, [images]);
+
   return (
     <ul className={classes.imageGallery}>
       {images.map(image => (
